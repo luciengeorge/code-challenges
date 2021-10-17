201df552955945e42379dd65fb6e5f023da32da1
Lucien George <luciengeorge95@gmail.com>
2021-10-17 21:56:58 +0100

    same tree problem

---
+++ binary_tree/same_tree.rb
@@ -0,0 +1,55 @@
+=begin
+Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+
+Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+
+Example 1:
+
+Input: p = [1,2,3], q = [1,2,3]
+Output: true
+
+Example 2:
+
+Input: p = [1,2], q = [1,null,2]
+Output: false
+
+Example 3:
+
+Input: p = [1,2,1], q = [1,1,2]
+Output: false
+
+Constraints:
+  The number of nodes in both trees is in the range [0, 100].
+  -104 <= Node.val <= 104
+=end
+
+# Definition for a binary tree node.
+# class TreeNode
+#   attr_accessor :val, :left, :right
+#   def initialize(val = 0, left = nil, right = nil)
+#     @val = val
+#     @left = left
+#     @right = right
+#   end
+# end
+# @param {TreeNode} p
+# @param {TreeNode} q
+# @return {Boolean}
+def collect(root)
+  queue = [root]
+  nodes = []
+  while queue.any?
+    node = queue.shift
+    nodes << node&.val
+
+    queue.append(node&.left)
+    queue.append(node&.right)
+  end
+  nodes
+end
+
+def is_same_tree(p, q)
+  nodes_p = collect(p)
+  nodes_q = collect(q)
+  nodes_p == nodes_q
+end
