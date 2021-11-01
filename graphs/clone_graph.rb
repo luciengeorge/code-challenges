e8b7816421aaae795e31df997c265d27ad179ff7
Lucien George <luciengeorge95@gmail.com>
2021-11-01 20:55:40 +0000

    renamed variable

--- graphs/clone_graph.rb
+++ graphs/clone_graph.rb
@@ -6,19 +6,19 @@ def clone_graph(node)
   return nil unless node
 
   queue = [node]
-  visited = { node => Node.new(node.val, []) }
+  cloned = { node => Node.new(node.val, []) }
 
   until queue.empty?
     current_node = queue.shift
 
     current_node.neighbors.each do |neighbor|
-      if visited[neighbor].nil?
-        visited[neighbor] = Node.new(neighbor.val, [])
+      if cloned[neighbor].nil?
+        cloned[neighbor] = Node.new(neighbor.val, [])
         queue.push(neighbor)
       end
 
-      visited[current_node].neighbors.push(visited[neighbor])
+      cloned[current_node].neighbors.push(cloned[neighbor])
     end
   end
-  visited[node]
+  cloned[node]
 end
